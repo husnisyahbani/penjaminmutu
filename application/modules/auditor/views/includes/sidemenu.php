ccfc2624269d1de54027314106a18064628072c0
M Husni Syahbani <husni.syahbani@gmail.com>
2025-10-24 16:01:14 +0700

    perbaikan route dan menu

--- application/modules/auditor/views/includes/sidemenu.php
+++ application/modules/auditor/views/includes/sidemenu.php
@@ -1,12 +1,12 @@
 <div class="site-menubar">
       <ul class="site-menu">
-        
-        <li class="site-menu-item <?php if(isset($dashboard)){echo $dashboard;} ?>">
-          <a class="animsition-link" href="<?php echo base_url($module);?>">
+        <li class="site-menu-item <?php if(isset($audit)){echo $audit;} ?>">
+          <a class="animsition-link" href="<?php echo base_url($module)."/daftaraudit";?>">
                   <i class="site-menu-icon md-view-compact" aria-hidden="true"></i>
-                  <span class="site-menu-title">Dashboard</span>
+                  <span class="site-menu-title">Daftar Audit</span>
               </a>
         </li>
+        
 
         <li class="site-menu-item has-sub <?php if(isset($data)){echo $data;} ?>">
           <a href="javascript:void(0)">
@@ -56,10 +56,10 @@
          </ul>
         </li>
 
-        <li class="site-menu-item <?php if(isset($audit)){echo $audit;} ?>">
-          <a class="animsition-link" href="<?php echo base_url($module)."/daftaraudit";?>">
+        <li class="site-menu-item <?php if(isset($dashboard)){echo $dashboard;} ?>">
+          <a class="animsition-link" href="<?php echo base_url($module."/dashboard");?>">
                   <i class="site-menu-icon md-view-compact" aria-hidden="true"></i>
-                  <span class="site-menu-title">Daftar Audit</span>
+                  <span class="site-menu-title">Formulir</span>
               </a>
         </li>
         
