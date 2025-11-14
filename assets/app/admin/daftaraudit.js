2937f48e788516f960ef86cfc2cb469f7ece0cf8
M Husni Syahbani <husni.syahbani@gmail.com>
2025-11-14 08:50:29 +0700

    perbaikan

--- assets/app/admin/daftaraudit.js
+++ assets/app/admin/daftaraudit.js
@@ -37,7 +37,7 @@ $(function () {
     $("#daftarpertanyaan").on("click", ".delik", function () {
         $dtform_id = $(this).attr('dtform_id');
         $audit_id = $(this).attr('audit_id');
-        window.location.href = base_url+'/delik?audit_id='+audit_id+"&dtform_id="+dtform_id;
+        window.location.href = base_url+"/delik?audit_id="+audit_id+"&dtform_id="+dtform_id;
     });
 
     $("#daftaraudit").on("click", ".detail", function () {
