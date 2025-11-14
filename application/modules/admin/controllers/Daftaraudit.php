ba818f7445f7dc64278de272c5a8ef833fd5a753
M Husni Syahbani <husni.syahbani@gmail.com>
2025-11-14 08:43:08 +0700

    perbaikan tag

--- application/modules/admin/controllers/Daftaraudit.php
+++ application/modules/admin/controllers/Daftaraudit.php
@@ -201,7 +201,7 @@ class Daftaraudit extends MY_Controller {
             $no++;
             $row = array();
             $row[] = $no;
-            $row[] = $field->dtform_pertanyaan."<br/".$field->dtform_lingkup;
+            $row[] = $field->dtform_pertanyaan."<br/>".$field->dtform_lingkup;
             $row[] = $field->jwb_jawaban;
             $row[] = ' <button type="button" class="delik btn btn-warning btn-xs waves-effect waves-classic" dtform_id=' . $field->dtform_id.' audit_id=' . $field->audit_id.'><i class="icon md-edit" aria-hidden="true"></i></button>';
            
