61e0a042cf5cdaecb7fa15725a707ee7336f1062
M Husni Syahbani <husni.syahbani@gmail.com>
2025-11-14 09:52:50 +0700

    perbaikan

--- application/modules/auditor/controllers/Daftaraudit.php
+++ application/modules/auditor/controllers/Daftaraudit.php
@@ -206,7 +206,12 @@ class Daftaraudit extends MY_Controller {
             $row[] = $field->dtform_pertanyaan."<br/>".$field->dtform_lingkup;
             //$jawaban = $this->truncate_words($field->jwb_jawaban);
            // $row[] = $field->jwb_jawaban;
-            $row[] = ' <button type="button" class="delik btn btn-warning btn-xs waves-effect waves-classic" dtform_id=' . $field->dtform_id.' audit_id=' . $field->audit_id.'><i class="icon md-edit" aria-hidden="true"></i>Lihat Hasil dan Delik</button>';
+           if(isset($field->jwb_jawaban)){
+                $row[] = ' <button type="button" class="delik btn btn-warning btn-xs waves-effect waves-classic" dtform_id=' . $field->dtform_id.' audit_id=' . $field->audit_id.'><i class="icon md-edit" aria-hidden="true"></i>Lihat Hasil dan Delik</button>';
+           }else{
+                $row[] = ' <button type="button" class="btn btn-danger btn-xs waves-effect waves-classic"><i class="icon md-edit" aria-hidden="true"></i>Auditee Tidak Menjawab</button>';
+           }
+            
            
             $data[] = $row;
         }
