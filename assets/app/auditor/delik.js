cad05ef0b118a5a261ebff25096077ef8a9feefd
M Husni Syahbani <husni.syahbani@gmail.com>
2025-11-16 08:07:01 +0700

    perbaikan auditor

--- assets/app/auditor/delik.js
+++ assets/app/auditor/delik.js
@@ -79,7 +79,7 @@ $("#editcatatan").on("click", function () {
 
     $("#kembali").on("click", function () {
     var id = $(this).attr('audit_id');
-    window.location.href = base_url+'/dashboard/detail/'+id;
+    window.location.href = base_url+'/daftaraudit/detail/'+id;
 });
 
 
