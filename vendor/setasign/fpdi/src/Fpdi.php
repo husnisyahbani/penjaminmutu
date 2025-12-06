6bb4dad9813d57a5fe9c605108706f77387106b8
M Husni Syahbani <husni.syahbani@gmail.com>
2025-12-07 06:06:58 +0700

    perbaikan

--- vendor/setasign/fpdi/src/Fpdi.php
+++ vendor/setasign/fpdi/src/Fpdi.php
@@ -32,49 +32,97 @@ class Fpdi extends FpdfTpl
      */
     const VERSION = '2.6.4';
 
-    function NbLines($w, $txt)
-{
-    global $pdf;
-    $cw = &$pdf->CurrentFont['cw'];
-    if($w==0)
-        $w = $pdf->w - $pdf->rMargin - $pdf->x;
-    $wmax = ($w - 2*$pdf->cMargin) * 1000 / $pdf->FontSize;
-    $s = str_replace("\r", '', $txt);
-    $nb = strlen($s);
-    if($nb>0 && $s[$nb-1]=="\n")
-        $nb--;
-    $sep = -1;
-    $i = 0;
-    $j = 0;
-    $l = 0;
-    $nl = 1;
-    while($i < $nb){
-        $c = $s[$i];
-        if($c == "\n"){
-            $i++;
-            $sep = -1;
-            $j = $i;
-            $l = 0;
-            $nl++;
-            continue;
-        }
-        if($c == ' ')
-            $sep = $i;
-        $l += $cw[$c];
-        if($l > $wmax){
-            if($sep == -1){
-                if($i == $j)
-                    $i++;
-            }else
-                $i = $sep + 1;
-            $sep = -1;
-            $j = $i;
-            $l = 0;
-            $nl++;
-        }else
-            $i++;
+    var $widths;
+    var $aligns;
+
+    function SetWidths($w)
+    {
+        $this->widths = $w;
     }
-    return $nl;
-}
+
+    function SetAligns($a)
+    {
+        $this->aligns = $a;
+    }
+
+    function Row($data)
+    {
+        $nb = 0;
+        for($i=0;$i<count($data);$i++)
+            $nb = max($nb, $this->NbLines($this->widths[$i], $data[$i]));
+        $h = 6 * $nb;
+
+        // Cek jika halaman habis
+        if($this->GetY() + $h > $this->PageBreakTrigger)
+            $this->AddPage($this->CurOrientation);
+
+        // Draw cells
+        for($i=0;$i<count($data);$i++)
+        {
+            $w = $this->widths[$i];
+            $a = isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+
+            // Save position
+            $x = $this->GetX();
+            $y = $this->GetY();
+
+            // Border
+            $this->Rect($x, $y, $w, $h);
+
+            // Content
+            $this->MultiCell($w, 6, $data[$i], 0, $a);
+
+            // Move right
+            $this->SetXY($x + $w, $y);
+        }
+
+        // Next line
+        $this->Ln($h);
+    }
+
+    function NbLines($w, $txt)
+    {
+        $cw = &$this->CurrentFont['cw'];
+        if($w == 0)
+            $w = $this->w - $this->rMargin - $this->x;
+        $wmax = ($w - 2*$this->cMargin) * 1000 / $this->FontSize;
+        $s = str_replace("\r", '', $txt);
+        $nb = strlen($s);
+        if($nb > 0 && $s[$nb-1] == "\n")
+            $nb--;
+        $sep = -1;
+        $i = 0;
+        $j = 0;
+        $l = 0;
+        $nl = 1;
+        while($i < $nb){
+            $c = $s[$i];
+            if($c == "\n"){
+                $i++;
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+                continue;
+            }
+            if($c == ' ')
+                $sep = $i;
+            $l += $cw[$c];
+            if($l > $wmax){
+                if($sep == -1){
+                    if($i == $j)
+                        $i++;
+                } else
+                    $i = $sep + 1;
+
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+            } else
+                $i++;
+        }
+        return $nl;
+    }
 
 }
