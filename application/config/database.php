1ef0fc24788a9c8fb5a4710a349047b113b76196
M Husni Syahbani <husni.syahbani@gmail.com>
2025-12-06 20:42:25 +0700

    tes

--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if (ENVIRONMENT === 'development') {
+if (ENVIRONMENT === 'production') {
     // 🔧 Koneksi lokal
     $db['default'] = array(
         'dsn'       => '',
