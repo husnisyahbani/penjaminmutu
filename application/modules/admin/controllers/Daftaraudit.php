9efcf3770b07714d184e916fb5e5b7d135e714a2
M Husni Syahbani <husni.syahbani@gmail.com>
2025-12-06 12:14:20 +0700

    tambahan template

--- application/modules/admin/controllers/Daftaraudit.php
+++ application/modules/admin/controllers/Daftaraudit.php
@@ -58,7 +58,7 @@ class Daftaraudit extends MY_Controller {
                 $pdf->AddPage();
                 $pdf->setSourceFile('./filedata/templatemutu.pdf');
                 // import page 1
-                $tplIdx = $pdf->importPage(1);
+                $tplIdx = $pdf->importPage(2);
                 $pdf->SetXY(0, 0);
                 // use the imported page and place it at position 10,10 with a width of 100 mm
                 $pdf->useTemplate($tplIdx, 0, 0, 297);
