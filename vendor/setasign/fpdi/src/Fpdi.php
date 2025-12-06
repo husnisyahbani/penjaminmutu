7c2d401e3ca91d9e2b9d4e9f49fb10d9bd7c98b2
M Husni Syahbani <husni.syahbani@gmail.com>
2025-12-06 20:52:21 +0700

    penambahan method

--- vendor/setasign/fpdi/src/Fpdi.php
+++ vendor/setasign/fpdi/src/Fpdi.php
@@ -31,4 +31,50 @@ class Fpdi extends FpdfTpl
      * @string
      */
     const VERSION = '2.6.4';
+
+    function NbLines($w, $txt)
+{
+    global $pdf;
+    $cw = &$pdf->CurrentFont['cw'];
+    if($w==0)
+        $w = $pdf->w - $pdf->rMargin - $pdf->x;
+    $wmax = ($w - 2*$pdf->cMargin) * 1000 / $pdf->FontSize;
+    $s = str_replace("\r", '', $txt);
+    $nb = strlen($s);
+    if($nb>0 && $s[$nb-1]=="\n")
+        $nb--;
+    $sep = -1;
+    $i = 0;
+    $j = 0;
+    $l = 0;
+    $nl = 1;
+    while($i < $nb){
+        $c = $s[$i];
+        if($c == "\n"){
+            $i++;
+            $sep = -1;
+            $j = $i;
+            $l = 0;
+            $nl++;
+            continue;
+        }
+        if($c == ' ')
+            $sep = $i;
+        $l += $cw[$c];
+        if($l > $wmax){
+            if($sep == -1){
+                if($i == $j)
+                    $i++;
+            }else
+                $i = $sep + 1;
+            $sep = -1;
+            $j = $i;
+            $l = 0;
+            $nl++;
+        }else
+            $i++;
+    }
+    return $nl;
+}
+
 }
