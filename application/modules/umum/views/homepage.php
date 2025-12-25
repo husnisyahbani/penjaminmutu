9a779798b38223a057917186d7470d81ad9a64c5
M Husni Syahbani <husni.syahbani@gmail.com>
2025-12-25 10:51:13 +0700

    perbaikan link

--- application/modules/umum/views/homepage.php
+++ application/modules/umum/views/homepage.php
@@ -496,7 +496,7 @@
         <div class="col-md-4 mb-4">
   <div class="card" style="border-radius:10px;overflow:hidden;box-shadow:0 5px 10px rgba(0,0,0,0.2);">
 
-    <img src="<?= $b['berita_file'] ?>" 
+    <img src="<?= base_url("filedata")."/".$b['berita_file'] ?>" 
          style="width:100%;height:220px;object-fit:cover;">
 
     <div class="card-body">
